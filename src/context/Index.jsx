f3e8245362554dcd70470f83911a6ad0930f3fb6
EufrasioJoao <EufrasioJoao00@gmail.com>
2022-12-21 20:05:18 +0200

    added white theme

--- src/context/Index.jsx
+++ src/context/Index.jsx
@@ -1,17 +1,14 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext } from "react";
 
 export const appContext = createContext(null);
 
 export function MainContext({ children }) {
-    // states
-    const [asideVisible, setasideVisible] = useState(true);
 
    
 
     // values to be shared across the context
     var contextValues = {
-        setasideVisible,
-        asideVisible,
+        usingContex: false
     };
 
     return (
